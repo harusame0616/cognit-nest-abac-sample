c910a5c29364a70855cb8d574ae04167d2dca576
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-03-04 12:37:40 +0900

    chore: amplify設定

--- web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "webf3e25752": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
