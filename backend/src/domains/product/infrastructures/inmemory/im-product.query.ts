9482a4206053f282443c7d5b1f149802b20ff7e0
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2023-03-08 13:33:11 +0900

    async と yarn.lock の漏れ修正 (#5)
    * chore: yarn.lock 追加
    
    * fix: async のコミット漏れ修正
    
    * chore: console.log削除

--- backend/src/domains/product/infrastructures/inmemory/im-product.query.ts
+++ backend/src/domains/product/infrastructures/inmemory/im-product.query.ts
@@ -2,17 +2,17 @@ import { products } from './product-store';
 import { ProductQuery } from 'src/domains/product/usecases/product-query-usecase';
 
 export class IMProductQuery implements ProductQuery {
-  findOneById(id: string) {
+  async findOneById(id: string) {
     const product = products.find((product) => product.id === id);
     return product ? { ...product } : null;
   }
 
-  findOneByStockId(stockId: string) {
+  async findOneByStockId(stockId: string) {
     const product = products.find((product) => product.stockId === stockId);
     return product ? { ...product } : null;
   }
 
-  list() {
+  async list() {
     return [...products];
   }
 }
--- backend/src/domains/stock/infrastructures/inmemory/im-stock.query.ts
+++ backend/src/domains/stock/infrastructures/inmemory/im-stock.query.ts
@@ -2,19 +2,17 @@ import { StockQuery } from '../../usecases/stock-query-usecase';
 import { stocks } from './stock-store';
 
 export class IMStockQuery implements StockQuery {
-  findOneById(id: string) {
+  async findOneById(id: string) {
     const stock = stocks.find((stock) => stock.id === id);
     return stock ? { ...stock } : null;
   }
 
-  findOneByProductId(
-    productId: string,
-  ): { id: string; quantity: number } | null {
+  async findOneByProductId(productId: string) {
     const stock = stocks.find((stock) => stock.productId === productId);
     return stock ? { ...stock } : null;
   }
 
-  list() {
+  async list() {
     return [...stocks];
   }
 }
