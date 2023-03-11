5406cffb9e5e9e3e5513a6702eed9917ffeb18fc
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-03-11 15:58:39 +0900

    feature: GraphQL の API プラグイン

---
+++ web/plugins/api/index.ts
@@ -0,0 +1,50 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const httpLink = createHttpLink({
+    // TODO: URL 動的にする
+    uri: 'http://localhost:3000/graphql',
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    if (!nuxtApp.$auth.user.value.isSignedIn) {
+      return headers;
+    }
+
+    const token = nuxtApp.$auth.user.value.credentials;
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return {
+    provide: {
+      api: client,
+    },
+  } as const;
+});
+
+declare module '#app' {
+  interface NuxtApp {
+    $api: typeof ApolloClient;
+  }
+}
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $api: typeof ApolloClient;
+  }
+}
